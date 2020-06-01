c154cbbb5b195631360e7172b80acaf0f1ae7a9f
endlesstrax <ricky@whitelionmedia.com>
2020-05-31 22:53:38 -0400

    Updated to pass all tests. Used custom cs50 library.

--- problem_set_6/mario.py
+++ problem_set_6/mario.py
@@ -1,22 +1,23 @@
+from cs50 import get_int
+
 def check_valid_int(input):
-    height = int(input)
     if height > 0 and height < 9:
         return False
     else:
         return True
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
 
-    height = input("Height: ")
+    height = get_int("Height: ")
 
     while check_valid_int(height):
-        height = input("Height: ")
-
+        height = get_int("Height: ")
+    
     hashes = 1
     height = int(height)
 
     while hashes <= height:
         spaces = height - hashes
-        print("." * spaces + "#" * hashes + "  " + "#" * hashes + "." * spaces)
+        print(' ' * spaces + '#' * hashes + '  ' + '#' * hashes)
         hashes = hashes + 1
