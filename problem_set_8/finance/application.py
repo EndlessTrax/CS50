de99d6bbb23a5d458fd29553978d1f2ea5af4bfd
endlesstrax <ricky@whitelionmedia.com>
2020-06-24 21:49:26 -0400

    Added register form to /register

--- problem_set_8/finance/application.py
+++ problem_set_8/finance/application.py
@@ -119,7 +119,7 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+    return render_template("register.html")
 
 
 @app.route("/sell", methods=["GET", "POST"])
