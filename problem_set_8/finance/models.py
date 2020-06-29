3d9f81a3a22eb657540363117e0952227a489b0e
endlesstrax <ricky@whitelionmedia.com>
2020-06-28 22:30:41 -0400

    Added transaction to model

--- problem_set_8/finance/models.py
+++ problem_set_8/finance/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
 from application import db
 
@@ -7,6 +9,20 @@ class Users(db.Model):
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(120), nullable=False)
     cash = db.Column(db.Integer, default=10000)
+    transactions = db.relationship('Transaction', backref='user', lazy=True)
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f"<User {self.username}>"
+
+
+class Transaction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    symbol = db.Column(db.String(10), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+    count = db.Column(db.Integer, nullable=False)
+    sold = db.Column(db.Boolean, default=False)
+    date_time = db.Column(db.DateTime, default=datetime.utcnow())
+
+    def __repr__(self):
+        return f"<Transaction {self.symbol}"
