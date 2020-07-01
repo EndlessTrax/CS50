e186b087d87a0cdcae530149df1819171b78c56e
endlesstrax <ricky@whitelionmedia.com>
2020-07-01 09:38:59 -0400

    cleane up unused imports + code. Painted black.

--- problem_set_8/finance/routes.py
+++ problem_set_8/finance/routes.py
@@ -1,11 +1,11 @@
 import os
 import requests
 
-from flask import flash, jsonify, redirect, render_template, request
+from flask import redirect, render_template, request
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 
-from helpers import apology, login_required, lookup, usd
+from helpers import apology, login_required
 from application import app, db, session
 from models import Users, Transaction
 
@@ -28,12 +28,17 @@ def index():
             name = stock.name
             count = stock.count
             price = requests.get(url.format(symbol, api_key)).json()["latestPrice"]
-            total = '{:.2f}'.format(price * count)
+            total = "{:.2f}".format(price * count)
 
             stocks.append([symbol.upper(), name, count, price, total])
             grand_total += float(total)
 
-    return render_template('index.html', stocks=stocks, cash=round(user.cash, 2), total=round(grand_total+user.cash, 2))
+    return render_template(
+        "index.html",
+        stocks=stocks,
+        cash=round(user.cash, 2),
+        total=round(grand_total + user.cash, 2),
+    )
 
 
 @app.route("/buy", methods=["GET", "POST"])
@@ -43,13 +48,15 @@ def buy():
     if request.method == "POST":
         symbol = request.form.get("symbol").lower()
         shares = int(request.form.get("shares"))
-        
+
         api_key = os.environ.get("API_KEY")
-        url = f"https://cloud-sse.iexapis.com/stable/stock/{symbol}/quote?token={api_key}"
+        url = (
+            f"https://cloud-sse.iexapis.com/stable/stock/{symbol}/quote?token={api_key}"
+        )
 
         r = requests.get(url)
         res = r.json()
-        
+
         if r.status_code != 200:
             return apology("An error occured")
 
@@ -58,11 +65,11 @@ def buy():
 
         if cost <= user.cash:
             transaction = Transaction(
-                    user_id = user.id,
-                    symbol = symbol.upper(),
-                    name = res["companyName"],
-                    price = res["latestPrice"],
-                    count = shares
+                user_id=user.id,
+                symbol=symbol.upper(),
+                name=res["companyName"],
+                price=res["latestPrice"],
+                count=shares,
             )
 
             user.cash = user.cash - (res["latestPrice"] * shares)
@@ -70,12 +77,12 @@ def buy():
             db.session.add(transaction)
             db.session.commit()
             db.session.close()
-            return redirect('/')
+            return redirect("/")
         else:
             return apology("Unable to complete transaction")
-    
-    else: # Method == GET
-        return render_template('buy.html')
+
+    else:  # Method == GET
+        return render_template("buy.html")
 
 
 @app.route("/history")
@@ -85,6 +92,7 @@ def history():
     user = Users.query.filter_by(id=session["user_id"]).first()
 
     return render_template("history.html", history=user.transactions)
+
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
@@ -103,10 +111,6 @@ def login():
         # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
-
-        # Query database for username
-        # rows = db.execute("SELECT * FROM users WHERE username = :username",
-        #                   username=request.form.get("username"))
 
         # Ensure username exists and password is correct
         user = Users.query.filter_by(username=request.form.get("username")).first()
@@ -143,16 +147,18 @@ def quote():
     if request.method == "POST":
         symbol = request.form.get("symbol").lower()
         api_key = os.environ.get("API_KEY")
-        url = f"https://cloud-sse.iexapis.com/stable/stock/{symbol}/quote?token={api_key}"
+        url = (
+            f"https://cloud-sse.iexapis.com/stable/stock/{symbol}/quote?token={api_key}"
+        )
 
         try:
             res = requests.get(url).json()
-            return render_template('quoted.html', data=res)
+            return render_template("quoted.html", data=res)
         except:
             return apology("Please enter a valid Symbol")
 
     else:
-        return render_template('quote.html')
+        return render_template("quote.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -162,17 +168,19 @@ def register():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403) 
+            return apology("must provide username", 403)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
             return apology("must provide password", 403)
-        
+
         # Ensure password and confirmation password match
         elif request.form.get("password") != request.form.get("confirmation"):
             return apology("passwords must match", 403)
 
-        user_query = Users.query.filter_by(username=request.form.get("username")).first()
+        user_query = Users.query.filter_by(
+            username=request.form.get("username")
+        ).first()
         if user_query:
             return apology("username already exists", 403)
 
@@ -180,7 +188,10 @@ def register():
         password_hash = generate_password_hash(request.form.get("password"))
 
         # Add to user to database
-        user = Users(username=request.form.get("username"), password_hash=request.form.get("password")) 
+        user = Users(
+            username=request.form.get("username"),
+            password_hash=request.form.get("password"),
+        )
         db.session.add(user)
         db.session.commit()
 
@@ -196,8 +207,8 @@ def register():
 def sell():
     """Sell shares of stock"""
     if request.method == "POST":
-        symbol_id = request.form.get('symbol')
-        shares = int(request.form.get('shares'))
+        symbol_id = request.form.get("symbol")
+        shares = int(request.form.get("shares"))
 
         if symbol_id == None or shares == None:
             return apology("Please fill in all fields")
@@ -207,7 +218,9 @@ def sell():
 
         api_key = os.environ.get("API_KEY")
         url = "https://cloud-sse.iexapis.com/stable/stock/{}/quote?token={}"
-        current_price = requests.get(url.format(stock.symbol, api_key)).json()["latestPrice"]
+        current_price = requests.get(url.format(stock.symbol, api_key)).json()[
+            "latestPrice"
+        ]
 
         user.cash += current_price
 
@@ -220,19 +233,19 @@ def sell():
 
         # Add a sold transaction to the database for historical records.
         sold_stock = Transaction(
-                user_id = user.id,
-                symbol = stock.symbol.upper(),
-                name = stock.name,
-                price = current_price,
-                count = shares,
-                sold = True
+            user_id=user.id,
+            symbol=stock.symbol.upper(),
+            name=stock.name,
+            price=current_price,
+            count=shares,
+            sold=True,
         )
 
         db.session.add(sold_stock)
         db.session.commit()
         db.session.close()
 
-        return redirect('/')
+        return redirect("/")
     else:
         user = Users.query.filter_by(id=session["user_id"]).first()
         transactions = user.transactions
@@ -240,9 +253,9 @@ def sell():
         list_of_symbols = []
 
         for stock in transactions:
-            list_of_symbols.append([stock.id, stock.symbol.upper()]) 
+            list_of_symbols.append([stock.id, stock.symbol.upper()])
 
-        return render_template('sell.html', symbols=list_of_symbols)
+        return render_template("sell.html", symbols=list_of_symbols)
 
 
 @app.route("/deposit", methods=["GET", "POST"])
@@ -250,14 +263,14 @@ def sell():
 def deposit():
     """Deposit more cash to account"""
     if request.method == "POST":
-        deposit_amount = int(request.form.get('amount'))
+        deposit_amount = int(request.form.get("amount"))
         user = Users.query.filter_by(id=session["user_id"]).first()
         user.cash += deposit_amount
         db.session.commit()
         db.session.close
-        return redirect('/')
-    else: 
-        return render_template('deposit.html')
+        return redirect("/")
+    else:
+        return render_template("deposit.html")
 
 
 def errorhandler(e):
