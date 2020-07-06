d9f9684e92fadf01fc42643e2a7fdd1f3ffc9d8b
endlesstrax <ricky@whitelionmedia.com>
2020-07-06 17:30:36 -0400

    Added working homepage

--- final_project/bonsai_market/settings.py
+++ final_project/bonsai_market/settings.py
@@ -40,6 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # My apps
+    'trees'
 ]
 
 MIDDLEWARE = [
@@ -57,7 +60,7 @@ ROOT_URLCONF = 'bonsai_market.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
