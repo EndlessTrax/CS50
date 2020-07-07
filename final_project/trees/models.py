2bc8e6f3d9c5919e4e2c853afa92b4fb90cddbe1
endlesstrax <ricky@whitelionmedia.com>
2020-07-06 22:36:13 -0400

    Added Tree model -  incomplete

--- final_project/trees/models.py
+++ final_project/trees/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+# Tree Model
+class Tree(models.Model):
+    LOCATION_CHOICES = [
+        ('VA', 'Virginia'),
+    ] # Move to own file when list is complete
+
+    name = models.CharField(max_length=80)
+    species = models.CharField(max_length=80) # drop max_length when drop down form complete
+    description = models.TextField()
+    location = models.CharField(max_length=2, choices=LOCATION_CHOICES)
+    price = models.PositiveIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    last_updated =models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id}: {self.name}"
+
+    class Meta:
+        ordering = ['-created']
+
