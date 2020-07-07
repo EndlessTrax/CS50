d3fff02c9eb1c35c61ac45d001591bce44243bfe
endlesstrax <ricky@whitelionmedia.com>
2020-07-07 14:20:46 -0400

    Added user creation test

--- final_project/users/tests.py
+++ final_project/users/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from .models import CustomUser, Transaction
 
-# Create your tests here.
+
+def test_user_creation(db):
+    user = CustomUser.objects.create_user(
+        username="testuser",
+        password="nottelling",
+        display_name="Test User"
+    )
+
+    assert user.username == "testuser"
+    assert user.check_password("nottelling")
+    assert user.display_name == "Test User"
