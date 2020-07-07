efa3a6b28e1a8cbc89ed2756e0f15f5b7300613b
endlesstrax <ricky@whitelionmedia.com>
2020-07-06 22:37:19 -0400

    Added Custom User and Transaction model added.

--- final_project/users/models.py
+++ final_project/users/models.py
@@ -1,3 +1,25 @@
+from typing import cast
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from trees.models import Tree
+
+
+# Custom User Model
+class CustomUser(AbstractUser):
+    transactions = models.ForeignKey(Transaction.id, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return self.username
+
+
+# Transaction Model
+class Transaction(models.Model):
+    user_id = models.OneToOneField(CustomUser.pk, on_delete=models.DO_NOTHING)
+    tree_id = models.OneToOneField(Tree.id, on_delete=models.DO_NOTHING)
+    sold = models.BooleanField(default=False)
+    bought = models.BooleanField(default=False)
+    date_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.id
