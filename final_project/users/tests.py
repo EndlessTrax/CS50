6a495965085e58fb882280da3e96cc809010fc00
endlesstrax <ricky@whitelionmedia.com>
2020-07-08 09:21:10 -0400

    Basic Transaction Test

--- final_project/users/tests.py
+++ final_project/users/tests.py
@@ -11,3 +11,15 @@ def test_user_creation(db):
     assert user.username == "testuser"
     assert user.check_password("nottelling")
     assert user.display_name == "Test User"
+
+
+
+def test_transaction_creation(db):
+    transaction = Transaction.objects.create(
+        user_id = 1,
+        tree_id = 1,
+        sold = True
+    )
+
+    assert transaction.tree_id == 1
+    assert transaction.sold == True
