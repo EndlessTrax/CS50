9481009a80a94aaa14ff462389a492372127fc7a
endlesstrax <ricky@whitelionmedia.com>
2020-07-09 12:49:35 -0400

    Updated user admin to use custome user forms.

--- final_project/users/admin.py
+++ final_project/users/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from .models import CustomUser, Transaction
+from .forms import CustomUserChangeForm, CustomUserCreationForm
 
 
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Transaction)
-
-admin.site.register(CustomUser)
