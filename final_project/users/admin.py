e1fca27402c06b01e0f21f73ded9e8e38e8a5a47
endlesstrax <ricky@whitelionmedia.com>
2020-07-09 12:02:00 -0400

    Added models to admin

--- final_project/users/admin.py
+++ final_project/users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import CustomUser, Transaction
+
+
+admin.site.register(Transaction)
+
+admin.site.register(CustomUser)
