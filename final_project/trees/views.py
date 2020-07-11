217fcbe7848eeee0c8c164f08499688b24970554
endlesstrax <ricky@whitelionmedia.com>
2020-07-11 12:02:28 -0400

    Add Tree View added

--- final_project/trees/views.py
+++ final_project/trees/views.py
@@ -1,5 +1,10 @@
 # from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, CreateView
+from django.urls import reverse_lazy
+
+
+# from .models import Tree
+from .forms import TreeCreationForm
 
 
 class HomePageView(TemplateView):
@@ -10,4 +15,7 @@ class MarketplacePageView(TemplateView):
     template_name = 'marketplace.html'
 
 
-
+class AddTreePageView(CreateView):
+    form_class = TreeCreationForm
+    success_url = reverse_lazy("dashboard")
+    template_name = "tree.html"
