a8fbbc2674a4418d6bca38f378e07e55e36a7da2
endlesstrax <ricky@whitelionmedia.com>
2020-07-10 22:00:20 -0400

    added primary key to profile url

--- final_project/users/urls.py
+++ final_project/users/urls.py
@@ -5,7 +5,7 @@ from .views import DashboardPageView, ProfilePageView, SignUpView
 
 urlpatterns = [
     path('dashboard/', DashboardPageView.as_view(), name='dashboard'),
-    path('profile/', ProfilePageView.as_view(), name='profile'),
+    path('profile/<int:pk>', ProfilePageView.as_view(), name='profile'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('signup/', SignUpView.as_view(), name='signup'),
 ]
