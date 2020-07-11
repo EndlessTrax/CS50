21afd819908e2369d26d6f2a1b20d62a8c465b7f
endlesstrax <ricky@whitelionmedia.com>
2020-07-11 12:03:06 -0400

    New tree route added

--- final_project/trees/urls.py
+++ final_project/trees/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import HomePageView, MarketplacePageView
+from .views import HomePageView, MarketplacePageView, AddTreePageView
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='homepage'),
     path('market/', MarketplacePageView.as_view(), name='marketplace'),
+    path('tree/new', AddTreePageView.as_view(), name='newtree'),
 ]
