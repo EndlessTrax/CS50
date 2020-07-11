6105e04e1dd5dea8fe06afe30b271628ddba75ca
endlesstrax <ricky@whitelionmedia.com>
2020-07-11 15:33:08 -0400

    Added single and edit tree templates, views and urls.

--- final_project/trees/urls.py
+++ final_project/trees/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
 
-from .views import HomePageView, MarketplacePageView, AddTreePageView
+from .views import (
+    HomePageView,
+    MarketplacePageView,
+    AddTreePageView,
+    SingleTreePageView,
+    EditTreePageView,
+)
 
 
 urlpatterns = [
-    path('', HomePageView.as_view(), name='homepage'),
-    path('market/', MarketplacePageView.as_view(), name='marketplace'),
-    path('tree/new', AddTreePageView.as_view(), name='newtree'),
+    path("", HomePageView.as_view(), name="homepage"),
+    path("market/", MarketplacePageView.as_view(), name="marketplace"),
+    path("tree/new/", AddTreePageView.as_view(), name="newtree"),
+    path("tree/<int:pk>/", SingleTreePageView.as_view(), name="single_tree"),
+    path("tree/edit/<int:pk>/", EditTreePageView.as_view(), name="edit_tree"),
 ]
--- final_project/trees/views.py
+++ final_project/trees/views.py
@@ -1,5 +1,6 @@
 # from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView
 from django.urls import reverse_lazy
 
 
@@ -15,7 +16,23 @@ class MarketplacePageView(ListView):
     template_name = 'marketplace.html'
     model = Tree
 
-class AddTreePageView(CreateView):
+
+class AddTreePageView(CreateView, LoginRequiredMixin):
     form_class = TreeCreationForm
     success_url = reverse_lazy("dashboard")
     template_name = "new_tree.html"
+
+
+class SingleTreePageView(DetailView):
+    template_name = "single_tree.html"
+    model = Tree
+
+
+class EditTreePageView(UpdateView, LoginRequiredMixin, UserPassesTestMixin):
+    template_name = 'edit_tree.html'
+    login_url = "login"
+    model = Tree
+    fields = ("name", "species", "description", "location", "price", "picture")
+
+    def get_success_url(self):
+        return reverse_lazy('dashboard')
