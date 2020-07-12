af909557ff09282c29833e26cb88b96fcbe027b9
endlesstrax <ricky@whitelionmedia.com>
2020-07-12 18:13:52 -0400

    Updated tree admin to display more then just the objects __str__

--- final_project/trees/admin.py
+++ final_project/trees/admin.py
@@ -3,4 +3,8 @@ from django.contrib import admin
 from .models import Tree
 
 
-admin.site.register(Tree)
+class TreeAdmin(admin.ModelAdmin):
+    list_display  = ['name', 'sold', 'owner_id']
+
+
+admin.site.register(Tree, TreeAdmin)
