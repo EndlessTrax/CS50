92bf8e418bd12cda745c43510d31c18425ddb990
endlesstrax <ricky@whitelionmedia.com>
2020-07-17 08:09:32 -0400

    Painted Black

--- final_project/trees/models.py
+++ final_project/trees/models.py
@@ -2,23 +2,27 @@ from django.conf import settings
 from django.db import models
 
 from .locations import LOCATION_CHOICES
-from users.models import CustomUser
+
 
 # Tree Model
 class Tree(models.Model):
     name = models.CharField(max_length=80)
-    species = models.CharField(max_length=80) # drop max_length when drop down form complete
+    species = models.CharField(
+        max_length=80
+    )  # drop max_length when drop down form complete
     description = models.TextField()
     location = models.CharField(max_length=2, choices=LOCATION_CHOICES)
     price = models.PositiveIntegerField()
     created = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
-    picture = models.ImageField(upload_to='trees/')
+    picture = models.ImageField(upload_to="trees/")
     sold = models.BooleanField(default=False)
-    owner_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
+    owner_id = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False
+    )
 
     def __str__(self):
         return f"{self.id}: {self.name}"
 
     class Meta:
-        ordering = ['-created']
+        ordering = ["-created"]
