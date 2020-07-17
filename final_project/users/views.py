0a28e9271e2dcefc5152d41a46bd761cbc48a977
endlesstrax <ricky@whitelionmedia.com>
2020-07-17 08:07:14 -0400

    Added real tree data to the users dashboard

--- final_project/users/views.py
+++ final_project/users/views.py
@@ -1,20 +1,17 @@
 # from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import TemplateView, CreateView, UpdateView, ListView
+from django.views.generic import TemplateView, CreateView, UpdateView
 from django.urls import reverse_lazy
 
 from .forms import CustomUserCreationForm
-from .models import CustomUser
+from .models import CustomUser, Transaction
+from trees.models import Tree
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
-
-
-class DashboardPageView(TemplateView, LoginRequiredMixin):
-    template_name = "dashboard.html"
 
 
 class ProfilePageView(UpdateView, LoginRequiredMixin, UserPassesTestMixin):
@@ -25,3 +22,34 @@ class ProfilePageView(UpdateView, LoginRequiredMixin, UserPassesTestMixin):
 
     def get_success_url(self):
         return reverse_lazy('dashboard')
+
+
+class DashboardPageView(TemplateView, LoginRequiredMixin):
+    template_name = "dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        trees_for_sale = Tree.objects.filter(owner_id=self.request.user.id)
+        user_transactions = Transaction.objects.filter(user_id=self.request.user.id)
+
+        transactions = list()
+        for item in user_transactions:
+            tree = Tree.objects.get(id=item.tree_id)
+
+            if tree.sold == True:
+                t_sale = "Sold"
+            else:
+                t_sale = "Bought"
+
+            transactions.append(dict(
+                tree_name = tree.name,
+                date = item.date_time,
+                price = tree.price,
+                sale = t_sale,
+            ))
+
+        context['trees_for_sale'] = trees_for_sale 
+        context['transactions'] = transactions
+
+        return context
