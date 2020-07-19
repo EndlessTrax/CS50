02a30b128c5d4dbdec620b492177b09bbd99bfb8
endlesstrax <ricky@whitelionmedia.com>
2020-07-19 16:19:00 -0400

    Added and updated tests

--- final_project/trees/tests.py
+++ final_project/trees/tests.py
@@ -1,14 +1,47 @@
+import pytest
 from .models import Tree
+from users.models import CustomUser
 
 
 def test_tree_creation(db):
+    user = CustomUser.objects.create_user(
+        username="testuser", 
+        password="nottelling", 
+        display_name="Test User"
+    )
     tree = Tree.objects.create(
         name="Old Juniper",
         species="Juniper",
         description="This is an old tree",
         location="VA",
         price=100,
-        picture='../static/imgs/juniper-bonsai.jpg'
+        picture='../static/imgs/juniper-bonsai.jpg',
+        owner_id=user
     )
 
     assert tree.name == "Old Juniper"
+    assert tree.owner_id.username == "testuser"
+
+
+def test_edit_a_tree(db):
+    user = CustomUser.objects.create_user(
+        username="testuser", 
+        password="nottelling", 
+        display_name="Test User"
+    )
+    tree = Tree.objects.create(
+        name="Old Juniper",
+        species="Juniper",
+        description="This is an old tree",
+        location="VA",
+        price=100,
+        picture='../static/imgs/juniper-bonsai.jpg',
+        owner_id=user
+
+    )
+
+    tree.price = 200
+    tree.description = "Its not that old, actually."
+
+    assert tree.price == 200
+    assert tree.description != "This is an old tree"
--- final_project/users/tests.py
+++ final_project/users/tests.py
@@ -1,11 +1,10 @@
 from .models import CustomUser, Transaction
+from .forms import CustomUserChangeForm
 
 
 def test_user_creation(db):
     user = CustomUser.objects.create_user(
-        username="testuser",
-        password="nottelling",
-        display_name="Test User"
+        username="testuser", password="nottelling", display_name="Test User"
     )
 
     assert user.username == "testuser"
@@ -13,13 +12,8 @@ def test_user_creation(db):
     assert user.display_name == "Test User"
 
 
-
 def test_transaction_creation(db):
-    transaction = Transaction.objects.create(
-        user_id = 1,
-        tree_id = 1,
-        sold = True
-    )
+    transaction = Transaction.objects.create(user_id=1, tree_id=1, sold=True)
 
     assert transaction.tree_id == 1
     assert transaction.sold == True
